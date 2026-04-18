c7c32ee5b441fb45e167a778bce0be0ae90b2faa
yod2004 <yodai0903@gmail.com>
2026-04-18 23:28:45 +0900

    Rxしたデータを適切に表示できるようにした

--- MINICOM/Core/Src/main.c
+++ MINICOM/Core/Src/main.c
@@ -120,18 +120,18 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
 		if(HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0,&RxHeader, RxData)!= HAL_OK){
 			Error_Handler();
 		}
-		id = RxHeader.Identifier;
-		if(id == 0x123){
-			CanRxData[0] = RxData[0];                                                    // Data
-			CanRxData[1] = RxData[1];
-			CanRxData[2] = RxData[2];
-			CanRxData[3] = RxData[3];
-			CanRxData[4] = RxData[4];
-			CanRxData[5] = RxData[5];
-			CanRxData[6] = RxData[6];
-			CanRxData[7] = RxData[7];
-			CanRxFlag = true;
-		}
+		CanRxIde = RxHeader.IdType;
+		CanRxId = RxHeader.Identifier;
+		CanRxDlc = RxHeader.DataLength;
+		CanRxData[0] = RxData[0];                                                    // Data
+		CanRxData[1] = RxData[1];
+		CanRxData[2] = RxData[2];
+		CanRxData[3] = RxData[3];
+		CanRxData[4] = RxData[4];
+		CanRxData[5] = RxData[5];
+		CanRxData[6] = RxData[6];
+		CanRxData[7] = RxData[7];
+		CanRxFlag = true;
 	}
 }
 
